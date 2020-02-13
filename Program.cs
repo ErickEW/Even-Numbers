614c9f1d2c7caf59b624f270b5fb9681da9f9322
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-02-13 15:26:44 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Even_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 10; i++)
+            if (i % 2 == 0){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
